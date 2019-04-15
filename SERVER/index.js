fc6541a0de25cd3ac9f018de0e9bd62ef31df0ce
Cory Bowers <n02360837@hawkmail.newpaltz.edu>
2019-04-15 10:49:04 -0400

    working on development server deep linking

--- SERVER/index.js
+++ SERVER/index.js
@@ -10,32 +10,40 @@ const port = 3000;
 // MIDDLE WEAR
 // below is cors adding response headers, telling browser to leave security to us
 
+//CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "Authorization");
   next();
 });
 
+app.use(express.static(path.join(__dirname, "../dist")));
+
+//AUTHORIZATION
 app.use(function(req, res, next) {
   try {
     const token = (req.headers.authorization || "").split(' ')[1]
     req.user = userModel.getFromToken(token);
   }
   catch (error) {
-    const openActions = ['POST/users', 'POST/users/login']
-    if(req.method != 'OPTIONS' && !openActions.includes(req.method, + req.path)){
+    const openActions = ['POST/users', 'POST/users/login', 'GET/myfriends', 'GET/register', 'GET/login', 'GET/about']
+    if(req.method != 'OPTIONS' && !openActions.includes(req.method, + req.path.toLowerCase)){
       next(Error("Login Required"));
     }
   }
   next();
 });
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../NoFramework")));
-app.get('/', (req, res) => res.send('Hello World!'));
+
 app.use("/users", users);
 
+//CATCH ALL FOR DEEP LINKING: not matter what is asked for, return index.html
+app.get("*", (req, res)=> res.sendFile(path.join(__dirname, "../dist/index.html")));
+
+//ERROR HANDLING
 app.use(function (err, req, res, next){
     console.error(err.stack)
     res.status(500).send({ message: err.message });
